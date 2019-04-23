a12eb511c12d0069f9cc0c7ee5b73df1c0d2158e
Muhammad Salman Abid <ealahherp@gmail.com>
2019-04-23 22:20:39 +0700

    video player

---
+++ app/src/main/java/com/shaikhutech/videoplayer/CustomItemClickListner.java
@@ -0,0 +1,9 @@
+package com.shaikhutech.videoplayer;
+
+import android.view.View;
+
+public interface CustomItemClickListner {
+    void onItemClick(View v, int postion);
+    void onItemLongClick(int position);
+
+}
